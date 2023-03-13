0769da8d195dd79f17c5e947917ad9ad7fe331d6
knotnapat <napat.wongchr@gmail.com>
2023-03-13 10:41:04 +0700

    feat: limiting responses

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ const app = express();
 const port = 4000;
 
 app.get("/posts", (req, res) => {
+  const limit = req.query.limit;
+
+  if (limit > 100) {
+    return res.status(401).json({
+      message: "Invalid request. Can get data up to 100 posts per request",
+    });
+  }
+
+  const blogPosts = blogPostMockDatabase.slice(0, limit);
+
   return res.json({
-    data: blogPostMockDatabase,
+    data: blogPosts,
   });
 });
 
